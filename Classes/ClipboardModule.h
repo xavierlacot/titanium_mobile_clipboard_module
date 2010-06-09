11d83b07ab5b4b01479af0883d163e1bd1c3bbfa
Xavier Lacot <xavier@lacot.org>
2010-06-09 16:25:13 +0200

    added module's code

---
+++ Classes/ClipboardModule.h
@@ -0,0 +1,13 @@
+/**
+ * @author Xavier Lacot <xavier@lacot.org>
+ *
+ * Appcelerator Titanium is Copyright (c) 2009-2010 by Appcelerator, Inc.
+ * and licensed under the Apache Public License (version 2)
+ */
+#import "TiModule.h"
+
+@interface ClipboardModule : TiModule
+{
+}
+
+@end
